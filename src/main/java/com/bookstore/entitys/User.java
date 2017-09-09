4608bc14b6681a1c8bdd0f5b7ed43dcc7c38ad88
ivan <ivan@itHome>
2017-09-09 09:58:05 +0300

    commit 9

--- src/main/java/com/bookstore/entitys/User.java
+++ src/main/java/com/bookstore/entitys/User.java
@@ -1,11 +1,9 @@
 package com.bookstore.entitys;
 
 import java.io.Serializable;
-import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
